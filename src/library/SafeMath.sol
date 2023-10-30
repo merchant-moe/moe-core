12170c0dc037589bdf279505b8b82c054abff6aa
0x0Louis <louis@joepegs.com>
2023-10-30 20:46:35 +0100

    cap SafeAdd to max(int256)
    This is to allow withdrawal in one step

--- src/library/SafeMath.sol
+++ src/library/SafeMath.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.13;
 library SafeMath {
     error SafeMath__Overflow();
 
+    uint256 internal constant MAX_INT256 = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+
     function addDelta(uint256 x, int256 delta) internal pure returns (uint256 y) {
         uint256 success;
 
         assembly {
             y := add(x, delta)
 
-            success := iszero(or(and(sgt(delta, 0), lt(y, x)), and(slt(delta, 0), gt(y, x))))
+            success := iszero(or(gt(x, MAX_INT256), gt(y, MAX_INT256)))
         }
 
         if (success == 0) revert SafeMath__Overflow();
--- test/library/SafeMath.t.sol
+++ test/library/SafeMath.t.sol
@@ -7,11 +7,8 @@ import "../../src/library/SafeMath.sol";
 
 contract SafeMathTest is Test {
     function test_fuzz_AddDelta(uint256 x, int256 delta) public returns (uint256 y) {
-        int256 edgeLow = x > uint256(type(int256).max) ? type(int256).min : int256(x);
-        int256 edgeHigh =
-            type(uint256).max - x > uint256(type(int256).max) ? type(int256).max : int256(type(uint256).max - x);
-
-        delta = bound(delta, edgeLow, edgeHigh);
+        x = bound(x, 0, uint256(type(int256).max));
+        delta = bound(delta, -int256(x), type(int256).max - int256(x));
 
         if (delta >= 0) {
             y = x + uint256(delta);
@@ -24,11 +21,13 @@ contract SafeMathTest is Test {
     }
 
     function test_fuzz_revert_AddDelta(uint256 x, int256 delta) public {
-        if (delta < 0) {
-            x = bound(x, 0, _abs(delta) - 1);
-        } else {
-            delta = delta == 0 ? int256(1) : delta;
-            x = bound(x, type(uint256).max - _abs(delta) + 1, type(uint256).max);
+        if (x <= uint256(type(int256).max)) {
+            if (delta < 0) {
+                x = bound(x, 0, _abs(delta) - 1);
+            } else {
+                delta = delta == 0 ? int256(1) : delta;
+                x = bound(x, uint256(type(int256).max) - uint256(delta) + 1, type(uint256).max);
+            }
         }
 
         vm.expectRevert(SafeMath.SafeMath__Overflow.selector);
