001749e22935bd23bdae26eaf61bc0e12f0947e8
0x0Louis <louis@joepegs.com>
2023-11-07 00:49:52 +0100

    clean up Moe

--- src/interface/IMoe.sol
+++ src/interface/IMoe.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IMoe is IERC20 {
+    error Moe__NotMinter(address account);
+
     function getMinter() external view returns (address);
     function getMaxSupply() external view returns (uint256);
     function mint(address account, uint256 amount) external returns (uint256);
