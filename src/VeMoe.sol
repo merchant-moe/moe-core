1ce62ce854dea42659f058c00a297ab565c667f0
0x0Louis <louis@joepegs.com>
2023-11-08 16:20:23 +0100

    update veMoe

--- src/VeMoe.sol
+++ src/VeMoe.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import {SafeMath} from "./library/SafeMath.sol";
 import {Rewarder} from "./library/Rewarder.sol";
@@ -9,20 +11,24 @@ import {Amounts} from "./library/Amounts.sol";
 import {Constants} from "./library/Constants.sol";
 import {IRewarder} from "./interface/IRewarder.sol";
 import {IMasterChef} from "./interface/IMasterChef.sol";
-import {IMultiRewarder} from "./interface/IMultiRewarder.sol";
+import {IRewarder} from "./interface/IRewarder.sol";
 import {IVeMoe} from "./interface/IVeMoe.sol";
 
-contract VeMoe is IVeMoe {
+contract VeMoe is Ownable, IVeMoe {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
     using Rewarder for Rewarder.Parameter;
     using Amounts for Amounts.Parameter;
+    using EnumerableSet for EnumerableSet.UintSet;
 
     IERC20 private immutable _moe;
     IMasterChef private immutable _masterChef;
 
     uint256 private _veMoePerSecond;
     uint256 private _maxVeMoePerMoe;
+
+    uint256 private _topPidsTotalVotes;
+    EnumerableSet.UintSet private _topPids;
 
     VeRewarder private _veRewarder;
     Reward[] private _rewards;
@@ -31,8 +37,9 @@ contract VeMoe is IVeMoe {
     Amounts.Parameter private _votes;
 
     mapping(address => User) private _users;
+    mapping(IRewarder => mapping(uint256 => uint256)) private _bribesTotalVotes;
 
-    constructor(IERC20 moe, IMasterChef masterChef) {
+    constructor(IERC20 moe, IMasterChef masterChef, address initialOwner) Ownable(initialOwner) {
         _moe = moe;
         _masterChef = masterChef;
     }
@@ -68,6 +75,34 @@ contract VeMoe is IVeMoe {
         return _votes.getTotalAmount();
     }
 
+    function getBribesTotalVotes(IRewarder bribe, uint256 pid) external view returns (uint256) {
+        return _bribesTotalVotes[bribe][pid];
+    }
+
+    function getBribesOf(address account, uint256 pid) external view returns (IRewarder) {
+        return _users[account].bribes[pid];
+    }
+
+    function getVotesOf(address account, uint256 pid) external view returns (uint256) {
+        return _users[account].votes.getAmountOf(pid);
+    }
+
+    function getTotalVotesOf(address account) external view returns (uint256) {
+        return _users[account].votes.getTotalAmount();
+    }
+
+    function getTopPoolIds() external view override returns (uint256[] memory) {
+        return _topPids.values();
+    }
+
+    function isInTopPoolIds(uint256 pid) external view override returns (bool) {
+        return _topPids.contains(pid);
+    }
+
+    function getTopPidsTotalVotes() external view returns (uint256) {
+        return _topPidsTotalVotes;
+    }
+
     function stake(uint256 amount) external {
         _modify(msg.sender, int256(amount));
 
@@ -85,12 +120,9 @@ contract VeMoe is IVeMoe {
     }
 
     function vote(uint256[] calldata pids, int256[] calldata deltaAmounts) external {
-        if (pids.length != deltaAmounts.length) {
-            revert VeMoe__InvalidLength();
-        }
         if (pids.length != deltaAmounts.length) revert VeMoe__InvalidLength();
 
-        _masterChef.updateAll();
+        _masterChef.updateAll(_topPids.values());
 
         User storage user = _users[msg.sender];
 
@@ -99,12 +131,24 @@ contract VeMoe is IVeMoe {
             uint256 pid = pids[i];
 
             (uint256 oldUserVotes, uint256 newUserVotes,,) = user.votes.update(pid, deltaAmount);
-            (,, uint256 oldTotalVotes,) = _votes.update(pid, deltaAmount);
+            _votes.update(pid, deltaAmount);
 
-            IMultiRewarder bribe = user.bribes[pid];
+            if (_topPids.contains(pid)) _topPidsTotalVotes.addDelta(deltaAmount);
+
+            if (deltaAmount >= 0) {
+                _bribesTotalVotes[user.bribes[pid]][pid] += uint256(deltaAmount);
+            } else {
+                _bribesTotalVotes[user.bribes[pid]][pid] -= uint256(-deltaAmount);
+            }
+
+            IRewarder bribe = user.bribes[pid];
 
             if (address(bribe) != address(0)) {
-                bribe.onModify(msg.sender, pid, oldUserVotes, newUserVotes, oldTotalVotes); // todo should use the totalVotes on this bribes, not the total
+                uint256 totalVotes = _bribesTotalVotes[bribe][pid];
+
+                _bribesTotalVotes[bribe][pid] = totalVotes.addDelta(deltaAmount);
+
+                bribe.onModify(msg.sender, pid, oldUserVotes, newUserVotes, totalVotes); // todo should use the totalVotes on this bribes, not the total
             }
         }
 
@@ -113,34 +157,57 @@ contract VeMoe is IVeMoe {
         emit Vote(msg.sender, pids, deltaAmounts);
     }
 
-    function setBribes(uint256[] calldata pids, IMultiRewarder[] calldata bribes) external {
+    function setBribes(uint256[] calldata pids, IRewarder[] calldata bribes) external {
         if (pids.length != bribes.length) revert VeMoe__InvalidLength();
 
         User storage user = _users[msg.sender];
 
         for (uint256 i; i < pids.length; ++i) {
             uint256 pid = pids[i];
-            IMultiRewarder newBribe = bribes[i];
 
-            IMultiRewarder oldBribe = user.bribes[pid];
+            IRewarder newBribe = bribes[i];
+            IRewarder oldBribe = user.bribes[pid];
 
             if (oldBribe == newBribe) continue;
 
             uint256 userVotes = user.votes.getAmountOf(pid);
-            uint256 totalVotes = _votes.getAmountOf(pid);
 
             user.bribes[pid] = newBribe;
 
             if (address(oldBribe) != address(0)) {
+                uint256 totalVotes = _bribesTotalVotes[oldBribe][pid];
+                _bribesTotalVotes[oldBribe][pid] = totalVotes - userVotes;
+
                 oldBribe.onModify(msg.sender, pid, userVotes, 0, totalVotes);
             }
-
             if (address(newBribe) != address(0)) {
+                uint256 totalVotes = _bribesTotalVotes[newBribe][pid];
+                _bribesTotalVotes[newBribe][pid] = totalVotes + userVotes;
+
                 newBribe.onModify(msg.sender, pid, 0, userVotes, totalVotes);
             }
         }
 
         emit BribesSet(msg.sender, pids, bribes);
+    }
+
+    function claimBribes(uint256[] calldata pids) external {
+        User storage user = _users[msg.sender];
+
+        for (uint256 i; i < pids.length; ++i) {
+            uint256 pid = pids[i];
+
+            IRewarder bribe = user.bribes[pid];
+
+            if (address(bribe) != address(0)) {
+                uint256 userVotes = user.votes.getAmountOf(pid);
+                uint256 totalVotes = _bribesTotalVotes[bribe][pid];
+
+                bribe.onModify(msg.sender, pid, userVotes, userVotes, totalVotes);
+            }
+        }
+
+        emit BribesSet(msg.sender, pids, new IRewarder[](pids.length));
     }
 
     function emergencyUnsetBribe(uint256[] calldata pids) external {
@@ -149,14 +216,42 @@ contract VeMoe is IVeMoe {
         for (uint256 i; i < pids.length; ++i) {
             uint256 pid = pids[i];
 
-            IMultiRewarder bribe = user.bribes[pid];
+            IRewarder bribe = user.bribes[pid];
 
             if (address(bribe) == address(0)) revert VeMoe__NoBribeForPid(pid);
+
+            uint256 userVotes = user.votes.getAmountOf(pid);
+            _bribesTotalVotes[bribe][pid] -= userVotes;
 
             delete user.bribes[pid];
         }
 
-        emit BribesSet(msg.sender, pids, new IMultiRewarder[](pids.length));
+        emit BribesSet(msg.sender, pids, new IRewarder[](pids.length));
+    }
+
+    function setTopPoolIds(uint256[] calldata pids) external onlyOwner {
+        uint256 length = pids.length;
+
+        if (length > Constants.MAX_NUMBER_OF_FARMS) revert VeMoe__TooManyPoolIds();
+
+        uint256 oldLength = _topPids.length();
+        while (oldLength > 0) {
+            _topPids.remove(_topPids.at(--oldLength));
+        }
+
+        uint256 totalVotes;
+        for (uint256 i; i < length; ++i) {
+            uint256 pid = pids[i];
+
+            if (!_topPids.add(pid)) revert VeMoe__InvalidLength();
+
+            uint256 votes = _votes.getAmountOf(pid);
+            totalVotes += votes;
+        }
+
+        _topPidsTotalVotes = totalVotes;
+
+        emit TopPoolIdsSet(pids);
     }
 
     function _modify(address account, int256 deltaAmount) private {
@@ -206,6 +301,8 @@ contract VeMoe is IVeMoe {
         uint256 newTotalSupply
     ) private {
         uint256 length = _rewards.length;
+
+        if (length == 0) return;
 
         uint256[] memory allRewards = new uint256[](length);
         address[] memory allTokens = new address[](length);
--- src/library/Constants.sol
+++ src/library/Constants.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.13;
 library Constants {
     uint256 internal constant PRECISION = 64;
     uint256 internal constant ACC_PRECISION_BITS = 64;
+
+    uint256 internal constant MAX_NUMBER_OF_FARMS = 32;
 }
