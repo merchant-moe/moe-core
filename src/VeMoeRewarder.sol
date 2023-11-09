0b9fd2229438e1fc80ba08f661afe53812fcd96b
0x0Louis <louis@joepegs.com>
2023-11-09 16:48:35 +0100

    create veMoe rewarder

---
+++ src/VeMoeRewarder.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {IVeMoe} from "./interface/IVeMoe.sol";
+import {SimpleRewarder, IRewarder} from "./SimpleRewarder.sol";
+
+contract VeMoeRewarder is SimpleRewarder {
+    error VeMoeRewarder__InvalidPid(uint256 pid);
+
+    uint256 internal immutable _pid;
+
+    constructor(IERC20 token, address caller, uint256 pid, address initialOwner)
+        SimpleRewarder(token, caller, initialOwner)
+    {
+        _pid = pid;
+    }
+
+    function onModify(address account, uint256 pid, uint256 oldBalance, uint256 newBalance, uint256 oldTotalSupply)
+        public
+        override
+    {
+        if (pid != _pid) revert VeMoeRewarder__InvalidPid(pid);
+
+        super.onModify(account, pid, oldBalance, newBalance, oldTotalSupply);
+    }
+
+    function _getTotalSupply() internal view override returns (uint256) {
+        return IVeMoe(_caller).getBribesTotalVotes(IRewarder(this), _pid);
+    }
+}
