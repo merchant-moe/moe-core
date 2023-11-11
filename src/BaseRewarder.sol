dcc32b880cfc83bc7e165eed4b98f0dec1337460
0x0Louis <louis@joepegs.com>
2023-11-11 19:27:00 +0100

    use ownable2step

--- src/BaseRewarder.sol
+++ src/BaseRewarder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import {Math} from "./library/Math.sol";
@@ -12,7 +12,7 @@ import {IBaseRewarder} from "./interface/IBaseRewarder.sol";
  * @title Base Rewarder Contract
  * @dev Base contract for reward distribution to stakers.
  */
-abstract contract BaseRewarder is Ownable, IBaseRewarder {
+abstract contract BaseRewarder is Ownable2Step, IBaseRewarder {
     using SafeERC20 for IERC20;
     using Math for uint256;
     using Rewarder for Rewarder.Parameter;
